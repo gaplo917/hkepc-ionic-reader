7d52488a2fc9ce99627325ea72079727a2765cd9
Gary Lo <logap@me.com>
2016-01-31 22:53:20 +0800

    hot fix the browser keyboard issue

--- src/es6/core/app.js
+++ src/es6/core/app.js
@@ -22,8 +22,8 @@ angular.module('starter', [
     // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
     // for form inputs)
     if (window.cordova && window.cordova.plugins && window.cordova.plugins.Keyboard) {
-      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true)
-      cordova.plugins.Keyboard.disableScroll(true)
+      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(false)
+      cordova.plugins.Keyboard.disableScroll(false)
 
     }
     if (window.StatusBar) {
