5fb9d67c3219c6062d5be68799231ccb65d3e002
Gary Lo <logap@me.com>
2016-02-07 15:47:33 +0800

    rewrite the search function in functional way

--- src/es6/core/controller/LikesController.js
+++ src/es6/core/controller/LikesController.js
@@ -11,17 +11,20 @@ export class LikesController{
     this.scope = $scope
     this.chats = []
     this.ngToast = ngToast
-    this.pageSize = 3
+    this.pageSize = 5
     this.sanitize = $sanitize
     this.end = false
-
-    // get the whole list from db
-    this.wholeMessages = $message.getAllLikedPost().reverse()
-
-    this.messages = this.wholeMessages.slice(0,this.pageSize)
-
     this.page = 1
-    this.totalPageNum = Math.ceil(this.wholeMessages.length / this.pageSize)
+
+    $scope.$on('$ionicView.enter', (e) => {
+      // get the whole list from db
+      this.wholeMessages = Object.assign([],$message.getAllLikedPost()).reverse()
+
+      this.messages = this.wholeMessages.slice(0,this.pageSize)
+
+      this.totalPageNum = Math.ceil(this.wholeMessages.length / this.pageSize)
+    })
+
   }
 
   loadMore(){
@@ -54,70 +57,84 @@ export class LikesController{
   }
 
   searchOnTextChange(){
-    function searchKeyword(str,keyword,indexArr = []){
+    function searchKeywordIndex(str,keyword,indexArr = []){
       const lastIndexPos = indexArr.length == 0 ? 0 : indexArr[indexArr.length - 1] + 1
       const index = str.indexOf(keyword,lastIndexPos)
       if(index == -1 || !str || !keyword){
         return indexArr
       } else {
-        return searchKeyword(str,keyword,indexArr.concat([index]))
+        return searchKeywordIndex(str,keyword,indexArr.concat([index]))
       }
     }
 
-    function searchBrace(str,indexArr = []) {
-      return (searchKeyword(str,'<').concat(searchKeyword(str,'>'))).sort((a,b) => a-b)
+    function searchBraceIndex(str,indexArr = []) {
+      return (searchKeywordIndex(str,'<').concat(searchKeywordIndex(str,'>'))).sort((a,b) => a - b)
     }
 
+    function isIndexInBrace(content,bracePos,index){
+      switch (bracePos.length){
+        case 0 :
+          return false
+        case 1 :
+          return false
+        default :
+          return (
+                  content[bracePos[0]] == '<'
+                  && content[bracePos[1]] == '>'
+                  && index > bracePos[0]
+                  && index < bracePos[1]
+                ) || isIndexInBrace(content,bracePos.slice(2),index)
+      }
+    }
+
+    function breakContent(content,len, validIndexs,prevPos = 0,splits = []) {
+      switch(validIndexs.length){
+        case 0:
+          // concat the rest of the content
+          return splits.concat([
+            content.slice(Math.min(prevPos,content.length)),
+          ])
+        default:
+          const contentIndex = validIndexs[0]
+          // break down into two part
+          const s = splits.concat([
+            content.slice(prevPos ,contentIndex),
+            content.slice(contentIndex,contentIndex + len),
+          ])
+          return breakContent(content,len,validIndexs.slice(1),contentIndex + len ,s)
+      }
+    }
+
+    function mergeAndInjectHightLightContent(splits,hlContent = ''){
+      switch (splits.length) {
+        case 0 :
+          return hlContent
+        case 1 :
+          return `${hlContent}${splits[0]}`
+        default :
+          const merged = `${hlContent}${splits[0]}<span style="background-color: yellow">${splits[1]}</span>`
+          return mergeAndInjectHightLightContent(splits.slice(2),merged)
+      }
+    }
 
     const result = this.wholeMessages.map( msg => {
       const content = angular.element('<div/>').html(msg.content).html();
       const keyword = this.searchText
 
       // find all brace for identify the html tag
-      const bracePos = searchBrace(content)
+      const bracePos = searchBraceIndex(content)
 
       // search the keyword position
-      const validIndex = searchKeyword(content.toLowerCase(),keyword.toLowerCase())
-                          .filter(index => {
+      const validIndex = searchKeywordIndex(content.toLowerCase(),keyword.toLowerCase())
+                          .filter(index => !isIndexInBrace(content,bracePos,index))
 
-                            // filter the position in the brace
-                            for(let i =0 ; i < bracePos.length ; i++){
-                              if(i < bracePos.length - 1 && content[bracePos[i]] == '<' && content[bracePos[i+1]] == '>' && index > bracePos[i] && index < bracePos[i + 1]){
-                                return false
-                              }
-                            }
-                            return true
-                          })
+      const splits = breakContent(content,keyword.length,validIndex)
 
+      // clone a new message object
       const hlm = Object.assign({},msg)
-      let splits = []
-      for(let i = 0; i < validIndex.length ; i++ ){
-        const contentIndex = validIndex[i]
-        const prevIndex = i == 0 ? 0 : validIndex[i - 1] + keyword.length
-        splits = splits.concat([
-          content.slice(prevIndex ,contentIndex),
-          content.slice(contentIndex,contentIndex + keyword.length),
-        ])
 
-        if(i == validIndex.length - 1){
-          splits = splits.concat([
-            content.slice(Math.min(contentIndex + keyword.length,content.length)),
-          ])
-        }
-
-      }
-
-      let hlContent = ''
-
-      for(let i = 0; i < splits.length ; i += 2){
-        if( i != splits.length - 1){
-          hlContent += `${splits[i]}<span style="background-color: yellow">${splits[i+1]}</span>`
-        }else {
-          hlContent+= `${splits[i]}`
-        }
-      }
-
-      hlm.content = hlContent
+      // set the content
+      hlm.content = mergeAndInjectHightLightContent(splits)
 
       return {
         hasMatch : validIndex.length > 0,
