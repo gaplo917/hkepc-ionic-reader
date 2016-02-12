166e9acb04146c2027b4e2315c8e4a401a2f11b7
Gary Lo <logap@me.com>
2016-02-13 01:52:24 +0800

    dynmaic import modules

--- src/es6/core/controllers.js
+++ src/es6/core/controllers.js
@@ -1,26 +1,10 @@
-import * as cheerio from "cheerio"
-import * as HKEPC from "../data/config/hkepc"
-import * as URLUtils from "../utils/url"
 import * as Controllers from './controller/index'
 
 /**
  * Register the controllers
  */
-angular.module('starter.controllers', [])
-.controller(Controllers.tab.name, Controllers.tab.action)
+const controllerModules = angular.module('starter.controllers', [])
 
-.controller(Controllers.topics.name, Controllers.topics.action)
-
-.controller(Controllers.posts.name, Controllers.posts.action)
-
-.controller(Controllers.post.name,Controllers.post.action)
-
-.controller(Controllers.chats.name, Controllers.chats.action)
-
-.controller(Controllers.chat.name, Controllers.chat.action)
-
-.controller(Controllers.auth.name,Controllers.auth.action)
-
-.controller(Controllers.notification.name,Controllers.notification.action)
-
-.controller(Controllers.likes.name,Controllers.likes.action)
+for(let key of Object.keys(Controllers)){
+  controllerModules.controller(Controllers[key].name,Controllers[key].action)
+}
