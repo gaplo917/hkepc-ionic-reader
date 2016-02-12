d78415d9d23d947f4f6c23800045e3a9d68ff6a5
Gary Lo <logap@me.com>
2016-02-13 01:52:24 +0800

    dynmaic import modules

--- src/es6/core/services.js
+++ src/es6/core/services.js
@@ -3,12 +3,8 @@ import * as Services from './service/index'
 /**
  * Register the service
  */
-angular.module('starter.services', [])
+const serviceModules = angular.module('starter.services', [])
 
-.factory(Services.localStorage.name,Services.localStorage.impl)
-
-.factory(Services.message.name,Services.message.impl)
-
-.factory(Services.AuthService.name,Services.AuthService.impl)
-
-.factory(Services.MessageResolver.name,Services.MessageResolver.impl)
+for(let key of Object.keys(Services)){
+  serviceModules.factory(Services[key].name,Services[key].impl)
+}
