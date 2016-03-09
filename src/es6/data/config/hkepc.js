5610687128ecb10510d27819bfcf307b3a0bb554
Gary Lo <logap@me.com>
2016-03-10 02:42:25 +0800

    bump version to 1.1

--- src/es6/data/config/hkepc.js
+++ src/es6/data/config/hkepc.js
@@ -4,7 +4,7 @@
 const PROXY_URL = 'https://ionic-reader.xyz'
 const BASE_URL = `http://www.hkepc.com/forum`
 const IMAGE_URL = 'http://www.hkepc.com/forum'
-const VERSION = "v1.0.0"
+const VERSION = "v1.1.0"
 function getMobileOperatingSystem(userAgent){
   const ua = userAgent || navigator.userAgent || navigator.vendor || window.opera;
 
