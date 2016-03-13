aee1d19e6b6f4f674c88bb382dde73b51031ab5f
Gary Lo <logap@me.com>
2016-03-13 21:37:13 +0800

    improve android build script

--- build-android.sh
+++ build-android.sh
@@ -9,3 +9,5 @@ jarsigner -verbose -sigalg SHA1withRSA -digestalg SHA1 -keystore hkepc-ir.keysto
 ~/Android/sdk/build-tools/21.1.1/zipalign -v 4 ./platforms/android/build/outputs/apk/android-x86-release-unsigned.apk ./platforms/android/build/outputs/apk/android-x86-release.apk;
 
 ~/Android/sdk/build-tools/21.1.1/zipalign -v 4 ./platforms/android/build/outputs/apk/android-armv7-release-unsigned.apk ./platforms/android/build/outputs/apk/android-armv7-release.apk;
+
+open ./platforms/android/build/outputs/apk/
