642bdd41d5de7f92998924752e3588f876b3198e
Gary Lo <logap@me.com>
2017-10-07 11:40:56 +0800

    add condition to not run unused code when it is a native app

--- src/es6/core/app.js
+++ src/es6/core/app.js
@@ -91,20 +91,27 @@ angular.module('starter', [
 
 })
 .config(['$httpProvider', function($httpProvider) {
-  // true if ionic is run in ios/android to allow use of cookies
-  if(isProxied){
-    $httpProvider.defaults.withCredentials = false
-  } else {
-    $httpProvider.defaults.withCredentials = true
+  if (window.WebViewJavascriptBridge){
+    // Native App No Need
+  }
+  else {
+    // true if ionic is run in ios/android to allow use of cookies
+    if(isProxied){
+      $httpProvider.defaults.withCredentials = false
+    } else {
+      $httpProvider.defaults.withCredentials = true
+    }
+
+    // always use async is a good practice
+    $httpProvider.useApplyAsync(true)
   }
 
-  // always use async is a good practice
-  $httpProvider.useApplyAsync(true)
 }])
 .config(['$ionicConfigProvider',function($ionicConfigProvider){
 
   $ionicConfigProvider.scrolling.jsScrolling(false)
-  $ionicConfigProvider.views.maxCache(5)
+  $ionicConfigProvider.views.forwardCache(false)
+  $ionicConfigProvider.views.maxCache(3)
   $ionicConfigProvider.spinner.icon('android')
   $ionicConfigProvider.tabs.style('standard')
   $ionicConfigProvider.tabs.position('bottom')
@@ -121,23 +128,26 @@ angular.module('starter', [
   $ionicConfigProvider.views.transition('ios')
 }])
 .provider('HKEPC_PROXY',[function(){
+  if (window.WebViewJavascriptBridge){
+    // Native App No Need
+  }
+  else {
+    this.$get = (rx, $q, ngToast, LocalStorageService) =>{
+      return {
+        request: function(config) {
+          var deferred = $q.defer()
 
-  this.$get = (rx, $q, ngToast, LocalStorageService) =>{
-    return {
-      request: function(config) {
-        var deferred = $q.defer()
-
-        rx.Observable.combineLatest(
-          LocalStorageService.get('proxy',HKEPC.proxy),
-          LocalStorageService.get(HKEPC.auth.id),
-          LocalStorageService.get(HKEPC.auth.token),
-          (proxyInDb, authId, token) => {
-            return {
-              proxyInDb: proxyInDb,
-              authId: authId,
-              token: token
-            }
-          }).subscribe(({proxyInDb, authId, token}) => {
+          rx.Observable.combineLatest(
+            LocalStorageService.get('proxy',HKEPC.proxy),
+            LocalStorageService.get(HKEPC.auth.id),
+            LocalStorageService.get(HKEPC.auth.token),
+            (proxyInDb, authId, token) => {
+              return {
+                proxyInDb: proxyInDb,
+                authId: authId,
+                token: token
+              }
+            }).subscribe(({proxyInDb, authId, token}) => {
             if(isProxied) {
               // we need to proxy all the request to prevent CORS
 
@@ -155,41 +165,48 @@ angular.module('starter', [
             config.timeout = 30000 // 30 seconds should be enough to transfer plain HTML text
 
             deferred.resolve(config)
-        }, (err) => {
-          deferred.resolve(config)
-        })
-
-        return deferred.promise
-      },
-      responseError: function(err){
-
-        if(err.status == 404){
-          ngToast.danger({
-            dismissOnTimeout: false,
-            content: `<i class="ion-network"> 找不到相關的內容！</i>`
+          }, (err) => {
+            deferred.resolve(config)
           })
-        }
-        else if (err.status == -1){
-          ngToast.danger({
-            dismissOnTimeout: true,
-            content: `<i class="ion-network"> 你的網絡不穩定，請重新嘗試！</i>`
-          })
-        }
-        else {
-          ngToast.danger({
-            dismissOnTimeout: true,
-            content: `<i class="ion-network"> 連線出現問題！有可能產生此問題的原因: 網絡不穩定、連線逾時、Proxy 伺服器出現異常</i>`
-          })
-        }
 
-        console.log('$http Error',JSON.stringify(err))
-        return err
+          return deferred.promise
+        },
+        responseError: function(err){
+
+          if(err.status == 404){
+            ngToast.danger({
+              dismissOnTimeout: false,
+              content: `<i class="ion-network"> 找不到相關的內容！</i>`
+            })
+          }
+          else if (err.status == -1){
+            ngToast.danger({
+              dismissOnTimeout: true,
+              content: `<i class="ion-network"> 你的網絡不穩定，請重新嘗試！</i>`
+            })
+          }
+          else {
+            ngToast.danger({
+              dismissOnTimeout: true,
+              content: `<i class="ion-network"> 連線出現問題！有可能產生此問題的原因: 網絡不穩定、連線逾時、Proxy 伺服器出現異常</i>`
+            })
+          }
+
+          console.log('$http Error',JSON.stringify(err))
+          return err
+        }
       }
     }
   }
+
 }])
 .config(function($httpProvider) {
-  $httpProvider.interceptors.push('HKEPC_PROXY')
+  if (window.WebViewJavascriptBridge){
+    // Native App No Need
+  }
+  else {
+    $httpProvider.interceptors.push('HKEPC_PROXY')
+  }
 })
 .config(['ngToastProvider', function(ngToast) {
   ngToast.configure({
@@ -200,13 +217,17 @@ angular.module('starter', [
   })
 }])
 .config(['$localForageProvider', function($localForageProvider){
-
-  $localForageProvider.config({
-    name        : 'HKEPCIR', // name of the database and prefix for your data, it is "lf" by default
-    version     : 1.0, // version of the database, you shouldn't have to use this
-    storeName   : 'keyvaluepairs', // name of the table
-    description : 'Simple persistant storage'
-  })
+  if (window.WebViewJavascriptBridge){
+    // Native App No Need
+  }
+  else {
+    $localForageProvider.config({
+      name        : 'HKEPCIR', // name of the database and prefix for your data, it is "lf" by default
+      version     : 1.0, // version of the database, you shouldn't have to use this
+      storeName   : 'keyvaluepairs', // name of the table
+      description : 'Simple persistant storage'
+    })
+  }
 
 }])
 
