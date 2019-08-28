924b3b7e0893d00bf0ab1578ea600001115ec463
Gary Lo <logap@me.com>
2019-08-28 16:17:03 +0800

    update readme that http proxy only used in local development

--- src/es6/data/config/hkepc.js
+++ src/es6/data/config/hkepc.js
@@ -1,7 +1,7 @@
 /**
  * Created by Gaplo917 on 9/1/2016.
 */
-const PROXY_URL = 'https://ionic-reader.xyz:8443'
+const PROXY_URL = 'http://0.0.0.0:1337/www.hkepc.com' // default proxy url (only local development use)
 const BASE_URL = `https://www.hkepc.com`
 const BASE_FORUM_URL = `${BASE_URL}/forum`
 const IMAGE_URL = 'https://www.hkepc.com/forum'
