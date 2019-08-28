e43c94cb694764e61d8d8aa47aecad1f9d6e0c11
Gary Lo <logap@me.com>
2019-08-28 16:08:45 +0800

    upgrade gulp to v4.0 for node 12+ remove unused dependencies

--- gulpfile.js
+++ gulpfile.js
@@ -1,24 +1,17 @@
-const browserify = require('browserify');
-const babelify = require('babelify');
-const gulp = require('gulp');
-const babel = require('gulp-babel');
-const concat = require('gulp-concat');
-const run = require('gulp-run');
+const browserify = require('browserify')
+const babelify = require('babelify')
+const gulp = require('gulp')
 const webserver = require('gulp-webserver')
-const stripDebug = require('gulp-strip-debug');
-const minify = require('gulp-minify');
-const sass = require('gulp-sass');
-const source = require('vinyl-source-stream');
-const buffer = require('vinyl-buffer');
-const gutil = require('gulp-util');
-const uglify = require('gulp-uglify');
-const sourcemaps = require('gulp-sourcemaps');
-const rename = require('gulp-rename')
-const ngAnnotate = require('gulp-ng-annotate');
+const stripDebug = require('gulp-strip-debug')
+const sass = require('gulp-sass')
+const source = require('vinyl-source-stream')
+const buffer = require('vinyl-buffer')
+const uglify = require('gulp-uglify')
+const sourcemaps = require('gulp-sourcemaps')
+const ngAnnotate = require('gulp-ng-annotate')
 const minifyCss = require('gulp-minify-css')
-const runSequence = require('run-sequence');
 
-function onError(err) {
+function onError (err) {
   console.log(err.message)
   this.emit('end')
 }
@@ -26,79 +19,72 @@ gulp.task('browserify', function () {
   // set up the browserify instance on a task basis
   const b = browserify({
     entries: './src/es6/core/app.js',
-    insertGlobals : true,
+    insertGlobals: true,
     debug: false,
     // defining transforms here will avoid crashing your stream
     transform: [
       babelify.configure({
-        plugins: ["transform-object-rest-spread","transform-async-to-generator"],
-        presets: ["es2015"]
+        plugins: ['transform-object-rest-spread', 'transform-async-to-generator'],
+        presets: ['es2015']
       })
     ]
   })
 
   return b
-      .bundle()
-      .on('error', onError)
-      .pipe(source('app.js'))
-      .pipe(buffer())
-      .pipe(sourcemaps.init({loadMaps: true}))
-      // Add transformation tasks to the pipeline here.
-      .pipe(sourcemaps.write('./'))
-      .pipe(gulp.dest('./www/js/'))
+    .bundle()
+    .on('error', onError)
+    .pipe(source('app.js'))
+    .pipe(buffer())
+    .pipe(sourcemaps.init({ loadMaps: true }))
+  // Add transformation tasks to the pipeline here.
+    .pipe(sourcemaps.write('./'))
+    .pipe(gulp.dest('./www/js/'))
 })
 
-
-gulp.task('webserver',() => {
+gulp.task('webserver', () => {
   return gulp.src('./www')
-      .pipe(webserver({
-        host: '0.0.0.0',
-        port: 3000,
-        livereload: true,
-        open: true,
-      }))
+    .pipe(webserver({
+      host: '0.0.0.0',
+      port: 3000,
+      livereload: true,
+      open: true
+    }))
 })
 
-gulp.task('sass', function() {
-  return gulp.src(['./src/scss/ionic.app.scss','./src/scss/ionic.app.dark.scss','./src/scss/ionic.app.oled.dark.scss'])
+gulp.task('sass', function () {
+  return gulp.src(['./src/scss/ionic.app.scss', './src/scss/ionic.app.dark.scss', './src/scss/ionic.app.oled.dark.scss'])
     .pipe(sass())
     .on('error', sass.logError)
     .pipe(gulp.dest('./www/css/'))
 })
 
-gulp.task('watchJs', function() {
-  return gulp.watch(['./src/es6/**/*'], ['browserify'])
+gulp.task('watchJs', function () {
+  return gulp.watch(['./src/es6/**/*'], gulp.series(['browserify']))
 })
 
-gulp.task('watchSass', function() {
-  return gulp.watch(['./src/scss/**/*'], ['sass'])
+gulp.task('watchSass', function () {
+  return gulp.watch(['./src/scss/**/*'], gulp.series(['sass']))
 })
 
-gulp.task('watch', ['watchJs','watchSass'])
+gulp.task('watch', gulp.parallel(['watchJs', 'watchSass']))
 
-gulp.task('compressCss', function() {
-  return gulp.src(['./www/css/ionic.app.css','./www/css/ionic.app.dark.css'])
-      .pipe(minifyCss({
-        keepSpecialComments: 0
-      }))
-      .pipe(gulp.dest('./www/css/'))
+gulp.task('compressCss', function () {
+  return gulp.src(['./www/css/ionic.app.css', './www/css/ionic.app.dark.css'])
+    .pipe(minifyCss({
+      keepSpecialComments: 0
+    }))
+    .pipe(gulp.dest('./www/css/'))
 })
-gulp.task('compressJs', function() {
+gulp.task('compressJs', function () {
   return gulp.src('./www/js/app.js')
-      .pipe(ngAnnotate())
-      .pipe(stripDebug())
-      .pipe(uglify({mangle: false}))
-      .pipe(gulp.dest('./www/js/'))
+    .pipe(ngAnnotate())
+    .pipe(stripDebug())
+    .pipe(uglify({ mangle: false }))
+    .pipe(gulp.dest('./www/js/'))
 })
 
-gulp.task('build', (cb) => {
-  return runSequence(['browserify','sass'],cb)
-})
+gulp.task('build', gulp.series(['browserify', 'sass']))
 
-gulp.task('release', (cb) => {
-  return runSequence('build',['compressJs','compressCss'],cb)
-})
+gulp.task('release', gulp.series(['compressJs', 'compressCss']))
 
-gulp.task('run', (cb) => {
-  return runSequence('build','watch','webserver',cb)
-})
+gulp.task('run', gulp.parallel(['build', 'watch', 'webserver']))
